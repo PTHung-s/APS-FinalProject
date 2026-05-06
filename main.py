567aacf1c6af8bf876ba6795260b6dfe8b23d2c8
Pham Tuan Hung <pthung310106@gmail.com>
2026-05-06 10:21:21 +0700

    create main file

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+from metropolis import metropolis
+from plot import plot_result
+
+def target_fn(x):
+    return np.exp(-x**2 / 2)
+
+def proposal_fn(x, stepsize):
+    return x + np.random.uniform(-stepsize, stepsize)
+
+def run(n=10000, x0=0.0, stepsize=1.0):
+    samples, acc_rate = metropolis(
+        n,
+        x0,
+        target_fn,
+        proposal_fn,
+        stepsize
+    )
+    plot_result(samples, acc_rate)
+
+if __name__ == "__main__":
+    run(10000)
